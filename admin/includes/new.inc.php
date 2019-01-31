f50be45fa7540458839bba46935d05d1ad1df2f4
mangl93 <32831350+mangl93@users.noreply.github.com>
2019-02-01 00:42:30 +0100

    01/02/2019

---
+++ admin/includes/new.inc.php
@@ -0,0 +1,76 @@
+<div class="container">
+
+  <div class="row">
+    
+   <div class="col-lg-12 text-left">
+
+     <h2 class="mt-5">Nuevo autor</h2>
+
+   </div>
+
+  </div>
+
+  <div class="row">
+    
+    <div class="col-lg-12 text-left">
+
+      <div class="col-lg-2 text-left">
+
+      </div>
+
+      <div class="col-lg-10 text-left">
+        
+        <form role="form" action="actions/new.act.php" method="post">
+
+          <div class="form-group row">
+
+
+            <label for="display_name" class="col-lg-2 col-form-label">Nombre</label>
+
+              <div class="col-lg-4 text-left">
+
+                <input type="text" class="form-control" id="display_name" name="display_name" placeholder>
+
+              </div>
+
+          </div>
+          <div class="form-group row">
+
+
+            <label for="display_name" class="col-lg-2 col-form-label">Email</label>
+
+              <div class="col-lg-4 text-left">
+
+                <input type="text" class="form-control" id="display_name" name="display_name" placeholder>
+
+              </div>
+              
+          </div>
+          <div class="form-group row">
+
+
+            <label for="display_name" class="col-lg-2 col-form-label">Contraseña</label>
+
+              <div class="col-lg-4 text-left">
+
+                <input type="password" class="form-control" id="display_name" name="display_name" placeholder>
+
+              </div>
+              
+          </div>
+
+
+        </form>
+
+      </div>
+
+      <div class="col-lg-2 text-left">
+
+      </div>
+
+
+    </div>
+
+  </div>  
+
+</div>
---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Thumbnail Gallery - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/estilos.css" rel="stylesheet">
+
+
+    <!-- Custom styles for this template -->
+    <link href="assets/startbootstrap/css/thumbnail-gallery.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Gallery</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="admin/index.php?page=new">ADMIN</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+
+    <!-- Page Content -->
+    
+      
+        
+        <div class="col-lg-3"></div>
+
+      </div>
+    <!-- /.container -->
+    </div>
+
+
+    <!-- Footer -->
+  
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+  </body>
+
+</html>
